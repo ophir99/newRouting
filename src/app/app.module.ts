395c2b563d30aed1b0e6b4ca024b34e6c59a2b97
Prasad Kadiyala <external.pvkadiyala@atheerair.com>
2019-01-24 21:55:15 +0530

    Simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { RouterModule } from "@angular/router";
+import { ErrComponent } from './err/err.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactComponent,
+    ErrComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      // {
+      //   path: "",
+      //   component: HomeComponent
+      // },
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'aboutus',
+        component: AboutusComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: "**",
+        component: ErrComponent
+        // redirectTo: 'contact',
+        // pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
